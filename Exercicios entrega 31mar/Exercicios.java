f9620dc5ddce7b773db1a8a25bf76d505da59ee5
gabrielMolin <gabriel.molin@dataway.info>
2023-03-30 00:24:45 -0700

    feat: lista de exercicios

---
+++ Exercicios entrega 31mar/Exercicios.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Exercicios{
+    public static void main(String[] args) {
+        // exercicioUm();
+        // exercicioDois();
+        // exercicioTres();
+        exercicioQuatro();
+        
+    }
+
+    //imprimir a soma do quadrado de 3 valores:
+    public static void exercicioUm(){ 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o primeiro valor:\n");
+        int valorUm = scanner.nextInt();
+        System.out.print("Digite o segundo valor:\n");
+        int valorDois = scanner.nextInt();
+        System.out.print("Digite o terceiro valor:\n");
+        int valorTres = scanner.nextInt();
+        scanner.close();
+
+        double somaDosQuadrados = Math.pow(valorUm, 2) + Math.pow(valorDois, 2) + Math.pow(valorTres, 2);
+        System.out.print("A soma dos quadrados é: " + Math.round(somaDosQuadrados));
+    }
+
+    //imprimir a soma do sucessor do triplo com o antecessor do dobro
+    public static void exercicioDois(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um valor:\n");
+        int valor = scanner.nextInt();
+        scanner.close();
+
+        int resultado = (valor * 3 + 1) + (valor * 2 - 1);
+        System.out.print("A soma do sucessor do triplo com o antecessor do dobro é: " + resultado);
+    }
+
+    //imprimir o valor recebido por um trabalhador
+    // valores: R$30,00 por dia. Remover 8% do desconto salarial
+    public static void exercicioTres(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o número de dias trabalhados no mês:\n");
+        int qntdDias = scanner.nextInt();
+        scanner.close();
+
+        if(qntdDias < 31){
+            double resultado = ((30.00 * qntdDias)/100) * 92;
+            System.out.print("O valor recebido será de: R$" + String.format("%.2f", resultado).replace(".", ","));
+        } else {
+            System.out.print("Quantidade de dias inválida.");
+        }
+    }
+
+    // Faça um programa que leia quanto cada apostador investiu, o valor do premio, e imprima quanto cada um ganharia do premio com base no valor investido.
+    public static void exercicioQuatro(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor investido pelo primeiro jogador:\n");
+        double valorUm = scanner.nextDouble();
+        System.out.print("Digite o valor investido pelo segundo jogador:\n");
+        double valorDois = scanner.nextDouble();
+        System.out.print("Digite o valor investido pelo terceiro jogador:\n");
+        double valorTres = scanner.nextDouble();
+        System.out.print("Digite o valor do prêmio:\n");
+        double valorPremio = scanner.nextDouble();
+        scanner.close();
+
+        double totalInvestido = valorUm + valorDois + valorTres;
+        double porcentagemUm = valorUm / totalInvestido;
+        double porcentagemDois = valorDois / totalInvestido;
+        double porcentagemTres = valorTres / totalInvestido;
+
+        double premioUm = porcentagemUm * valorPremio;
+        double premioDois = porcentagemDois * valorPremio;
+        double premioTres = porcentagemTres * valorPremio;
+
+        System.out.print(
+            "O primeiro jogador recebe: R$" +  String.format("%.2f", premioUm).replace(".", ",") +
+            "\nO segundo jogador recebe: R$" +  String.format("%.2f", premioDois).replace(".", ",") +
+            "\nO terceiro jogador recebe: R$" +  String.format("%.2f", premioTres).replace(".", ",")
+        );
+    }
+}
