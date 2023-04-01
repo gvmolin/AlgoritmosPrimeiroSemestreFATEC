d8d8708bb19f437b5811f66761afbbca93f85c12
gabrielMolin <gabriel.molin@dataway.info>
2023-03-31 18:37:34 -0700

    feat: finalizado

--- Exercicios entrega 31mar/Exercicios.java
+++ Exercicios entrega 31mar/Exercicios.java
@@ -8,7 +8,9 @@ public class Exercicios{
         // exercicioQuatro();
         // exercicioCinco();
         // exercicioSeis();
-        exercicioSeisMelhorado();
+        // exercicioSeisMelhorado();
+        // exercicioSete();
+        exercicioOito();
         
     }
 
@@ -107,6 +109,7 @@ public class Exercicios{
         System.out.print("A média final é de: " + media + "\n O aluno está " + status);
     }
 
+    //Usando switch, escreva um programa que leia um inteiro entre 1 e 12 e imprima o mes
     public static void exercicioSeis(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Descubra qual mês é correspondente ao número escolhido de 1 a 12:\n");
@@ -176,5 +179,62 @@ public class Exercicios{
         }
     }
 
-    
+    //Escreva o menu de opcoes abaixo. Leia a opcão do usuario e execute a operacão ao escolhida. Escreva uma mensagem de erro se a opcçao for invalida.
+    public static void exercicioSete(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("CACULADORA\nDigite o número operaçao desejada:\n1- Soma de 2 números.\n2- Diferença entre 2 números (maior pelo menor)\n3- Produto entre 2 números.\n4- Divisao entre 2 números (o denominador nao pode ser zero).\nOPÇAO:");
+        int opc = scanner.nextInt();
+        if(opc > 4){
+            scanner.close();
+            System.out.print("Opçao inválida, tente novamente.");
+            return;
+        }
+        System.out.print("Digite o primeiro valor:");
+        double valor1 = scanner.nextDouble();
+        System.out.print("Digite o segundo valor:");
+        double valor2 = scanner.nextDouble();
+        scanner.close();
+
+        double result = 0;
+        if(opc == 1){
+            result = valor1 + valor2;
+        } else if (opc == 2){
+            double valorMenor, valorMaior;
+            valorMaior = (valor1 >= valor2 ? valor1 : valor2);
+            valorMenor = (valor1 >= valor2 ? valor2 : valor1);
+            result = valorMaior - valorMenor;
+        } else if (opc == 3){
+            result = valor1 * valor2;
+        } else if (opc == 4 && valor2 != 0){
+            result = valor1 / valor2;
+        } else if (opc == 4 && valor2 == 0){
+            System.out.print("O denominador deve ser diferente de zero.");
+            return;
+        }
+
+        System.out.print("O resultado da operaçao é: " +  result);
+    }
+
+    //calcule o consumo em Km/l
+    public static void exercicioOito(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o número de km rodados:");
+        int km = scanner.nextInt();
+        System.out.print("Digite a quantidade de gasolina consumida:");
+        double l = scanner.nextDouble();
+        scanner.close();
+
+        double consumo = km/l;
+        String mensagem;
+
+        if(consumo < 8){
+            mensagem = "Venda o carro!";
+        } else if(consumo >= 8 && consumo <= 12){
+            mensagem = "Econômico!";
+        } else {
+            mensagem = "Super econômico!";
+        }
+
+        System.out.print(mensagem);
+    }
 }
